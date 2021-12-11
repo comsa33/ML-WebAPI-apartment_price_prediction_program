4240bfd559824a5c6c8c188159c517a6da83fe87
Ruo Lee <ruo@Ruos-MacBook-Pro.local>
2021-12-11 17:01:41 +0900

    v2.0

--- My_apt/realestate_all_api.py
+++ My_apt/realestate_all_api.py
@@ -6,7 +6,7 @@ Encoding = "F9JRa0SmO1YC0LOweI2DUZO%2BLuqgg3ckPbplWoFkaQkVHLC87cgFrWPLq8476LXHiI
 Decoding = "F9JRa0SmO1YC0LOweI2DUZO+Luqgg3ckPbplWoFkaQkVHLC87cgFrWPLq8476LXHiId5e0jGCQvwsOCffNrN1Q=="
 
 url = 'http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev'
-params ={'serviceKey' : Decoding, 'pageNo' : '1', 'numOfRows' : '10', 'LAWD_CD' : '11110', 'DEAL_YMD' : '201512' }
+params ={'ServiceKey' : Encoding, 'pageNo' : '1', 'numOfRows' : '10', 'LAWD_CD' : '11110', 'DEAL_YMD' : '201512' }
 
 response = requests.get(url, params=params)
 print(response.content)
