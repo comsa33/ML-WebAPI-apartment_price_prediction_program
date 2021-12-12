71f8e0070c510a8820358443aecdb22926e67545
Ruo Lee <ruo@Ruos-MacBook-Pro.local>
2021-12-12 17:37:29 +0900

    add video

--- My_apt/realestate_all_api.py
+++ My_apt/realestate_all_api.py
@@ -1,12 +1,15 @@
 import requests
 import xmltodict, json
-
+from pprint import pprint
 
 Encoding = "F9JRa0SmO1YC0LOweI2DUZO%2BLuqgg3ckPbplWoFkaQkVHLC87cgFrWPLq8476LXHiId5e0jGCQvwsOCffNrN1Q%3D%3D"
 Decoding = "F9JRa0SmO1YC0LOweI2DUZO+Luqgg3ckPbplWoFkaQkVHLC87cgFrWPLq8476LXHiId5e0jGCQvwsOCffNrN1Q=="
 
 url = 'http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev'
-params ={'ServiceKey' : Encoding, 'pageNo' : '1', 'numOfRows' : '10', 'LAWD_CD' : '11110', 'DEAL_YMD' : '201512' }
+params ={'ServiceKey' : Encoding, 'pageNo' : '1', 'numOfRows' : '1', 'LAWD_CD' : '11110', 'DEAL_YMD' : '202011' }
 
 response = requests.get(url, params=params)
-print(response.content)
+obj = xmltodict.parse(response.content)
+result = json.dumps(obj, ensure_ascii=False)
+pprint(result)
+
